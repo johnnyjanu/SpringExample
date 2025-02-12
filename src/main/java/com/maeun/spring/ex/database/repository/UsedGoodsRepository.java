f357d144967934963a650848c24aca31ebb9acf4
johnnyjanu <juni20030703@gmail.com>
2025-02-12 19:58:54 +0900

    [권혁준] - database 연동(mybatis) 예제 완료

--- src/main/java/com/maeun/spring/ex/database/repository/UsedGoodsRepository.java
+++ src/main/java/com/maeun/spring/ex/database/repository/UsedGoodsRepository.java
@@ -2,11 +2,14 @@ package com.maeun.spring.ex.database.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.maeun.spring.ex.database.domain.UsedGoods;
 
 // 데이터베이스 관련 처리
 // Mybatis Framework
-public interface UsedGoodsRespository {
+@Mapper
+public interface UsedGoodsRepository {
 	
 	// used_goods 테이블 모든 행 조회
 	public List<UsedGoods> selectUsedGoodsList();
--- src/main/java/com/maeun/spring/ex/database/service/UsedGoodsService.java
+++ src/main/java/com/maeun/spring/ex/database/service/UsedGoodsService.java
@@ -1,6 +1,12 @@
 package com.maeun.spring.ex.database.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.maeun.spring.ex.database.domain.UsedGoods;
+import com.maeun.spring.ex.database.repository.UsedGoodsRepository;
 
 // 로직(business) 담당
 // 데이터 가공
@@ -9,11 +15,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsedGoodsService {
 	
+	// 해당 멤버변수에 Spring이 자동으로 객체를 생성해서 <주입>해준다!
+	@Autowired
+	private UsedGoodsRepository usedGoodsRepository;
+	
 	// 중고물품 게시글 모두 얻어오기
-	public getUsedGoods() {
+	public List<UsedGoods> getUsedGoodsList() {
 		// used_goods 테이블 모든 행 조회
-		
-		
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		// 데이터 가공
+		return usedGoodsList;
 	}
 	
 }
