1a85fab9c11eb701259455223b0f5042373d6247
johnnyjanu <juni20030703@gmail.com>
2025-02-17 21:48:04 +0900

    [권혁준] - MVC Cycle 예제 완료

---
+++ src/main/java/com/maeun/spring/ex/mvc/service/UserService.java
@@ -0,0 +1,29 @@
+package com.maeun.spring.ex.mvc.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maeun.spring.ex.mvc.domain.User;
+import com.maeun.spring.ex.mvc.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	// 사용자 정보를 전달 받고 저장기능
+	public int addUser(
+			String name
+			, String birthday
+			, String email
+			, String introduce) {
+		return userRepository.insertUser(name, birthday, email, introduce);
+	}
+	
+	public User getLastUser() {
+		return userRepository.selectLastUser();
+	}
+	
+	
+}
