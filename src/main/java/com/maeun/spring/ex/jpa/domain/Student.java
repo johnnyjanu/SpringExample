42154340921c52550962d779846972c77451314a
johnnyjanu <juni20030703@gmail.com>
2025-02-28 21:45:23 +0900

    [권혁준] - jpa lomgok 빌드 패턴

---
+++ src/main/java/com/maeun/spring/ex/jpa/domain/Student.java
@@ -0,0 +1,23 @@
+package com.maeun.spring.ex.jpa.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+@Setter
+public class Student {	
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String dreamJob;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+}
